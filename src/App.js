f0012fc79790b3402e9044b358f28fc96482ddf5
Mateus Alencar <mtauws@gmail.com>
2020-07-18 20:27:49 -0300

    Component Weather

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Wheater App</h1>
-    </div>
-  );
+import 'weather-icons/css/weather-icons.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import Weather from './components/weather'
+
+//api call api.openweathermap.org/data/2.5/weather?q=London,uk
+const API_KEY = "b718feefc076806fb97c63bb4ac8dfd3"
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {}
+    this.getWeather()
+  }
+
+  getWeather = async () => {
+    const api_call = await fetch(`api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${API_KEY}`)
+    
+    const response = await api_call.json()
+
+    console.log(response)
+  }
+
+  render() {
+    return(
+      <div className="App">
+        <Weather />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/weather.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Weather = () => {
+  return (
+    <div className="container">
+      <div className="cards">
+        <h1>London</h1>
+        <h5 className="py-4">
+          <i className="wi wi-day-sunny display-1"></i>
+        </h5>
+        <h1 className="py2">25&deg;</h1>
+
+        {/** show max and min temp */}
+        {minMaxTemp(24, 19)}
+      </div>
+    </div>
+  );
+}
+
+function minMaxTemp(min, max) {
+  return (
+    <h3>
+      <span className="px-4">{min}&deg;</span>
+      <span className="px-4">{max}&deg;</span>
+    </h3>
+  )
+}
+
+export default Weather
